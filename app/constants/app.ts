6521f664f49325a56ae3724f47e298fe95805c22
sohammishra864-wq <sohammishra864@gmail.com>
2026-09-11 21:56:05 +0530

    constants

---
+++ app/constants/app.ts
@@ -0,0 +1,31 @@
+export const APP_NAME = 'TrustMate';
+
+export const AGENT_PHONE_NO = '+15106501647';
+
+// English + Hinglish, then the 22 official languages (8th Schedule).
+export const INDIAN_LANGUAGES = [
+  'English',
+  'Hinglish',
+  'Hindi',
+  'Bengali',
+  'Marathi',
+  'Telugu',
+  'Tamil',
+  'Gujarati',
+  'Urdu',
+  'Kannada',
+  'Odia',
+  'Malayalam',
+  'Punjabi',
+  'Assamese',
+  'Maithili',
+  'Santali',
+  'Kashmiri',
+  'Nepali',
+  'Konkani',
+  'Sindhi',
+  'Dogri',
+  'Manipuri',
+  'Bodo',
+  'Sanskrit',
+];
